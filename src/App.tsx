e40b4573a2a19b171d4dec04ce104b708c7f72af
Aiman03at <aiman03at@gmail.com>
2026-01-24 19:12:06 -0500

    Day1 -React +TS portforlio Foundation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+
+ function App() {
+  
+
+   
+  return (<div className="min-h-screen flex items-center justify-center bg-white">
+    <h1 className="text-4xl font-bold text-slate-900">Aiman's Portfolio ---- Coming Soon</h1></div>)
+}
+
+export default App;
